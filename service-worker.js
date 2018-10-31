7528a26cb9d86fb8a6e3328cb14b6244c5c16495
Maggie <maggievu91@gmail.com>
2018-10-31 02:17:32 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/react-landing-page/precache-manifest.d66235f4abf5486971a8ad287a245401.js"
+  "/react-landing-page/precache-manifest.203db6d6bfb0804c26acef95d6b4e9ca.js"
 );
 
 workbox.clientsClaim();
