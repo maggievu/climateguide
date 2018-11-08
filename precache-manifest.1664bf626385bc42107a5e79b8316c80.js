e7fd4ca48e4bf44aa9d3b94a081e9e6ea32d75be
Maggie <maggievu91@gmail.com>
2018-11-07 23:22:30 -0800

    Updates

--- precache-manifest.1664bf626385bc42107a5e79b8316c80.js
+++ precache-manifest.1664bf626385bc42107a5e79b8316c80.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/climateguide/static/js/runtime~main.b51b14d5.js"
   },
   {
-    "revision": "a9981bf34e625d66ca56",
-    "url": "/climateguide/static/js/main.a9981bf3.chunk.js"
+    "revision": "a209d3a9dfa7a0594a7c",
+    "url": "/climateguide/static/js/main.a209d3a9.chunk.js"
   },
   {
     "revision": "7616deaf3996b6d72148",
     "url": "/climateguide/static/js/1.7616deaf.chunk.js"
   },
   {
-    "revision": "a9981bf34e625d66ca56",
-    "url": "/climateguide/static/css/main.9a8a3591.chunk.css"
+    "revision": "a209d3a9dfa7a0594a7c",
+    "url": "/climateguide/static/css/main.874ff529.chunk.css"
   },
   {
-    "revision": "a5b8fd31ac82f46242d5952b29b86ecc",
+    "revision": "77e62f2222d909ce580f62c6ec60102f",
     "url": "/climateguide/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/climateguide/precache-manifest.7761750af3127772bb4c7aa7793218a0.js"
+  "/climateguide/precache-manifest.1664bf626385bc42107a5e79b8316c80.js"
 );
 
 workbox.clientsClaim();
